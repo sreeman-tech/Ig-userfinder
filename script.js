c32d73551cebff5004343f219d2bf04f821b0442
sreeman <114044895+sreeman-tech@users.noreply.github.com>
2023-01-03 21:20:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$("#form").submit((e) => {
+
+    e.preventDefault()
+
+ 
+
+    let username = $("#profile").val()
+
+ 
+
+    $.instagramFeed({
+
+        'username':username,
+
+        'container':"#instagram-feed-demo"
+
+    })
+
+})
+
